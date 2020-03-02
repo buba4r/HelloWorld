22d83c12e996d596d178d55f75f2e31ceeba5d03
buba4r <bubakar24@gmail.com>
2020-03-02 00:25:15 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args){
-    System.out.println("Hello , World"); // comm for version 10
+    System.out.println("Hello , Boubacar"); // comm for version 10
   }
 
 }
